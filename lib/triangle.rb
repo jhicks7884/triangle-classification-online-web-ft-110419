f6298c5b9fdfd506fda2863f962456c64882d1e2
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-26 16:41:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
+
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize
+    @equilateral = equilateral
+  end
   # write code here
+
+
+  class TriangleError < StandardError
+  end
 end
