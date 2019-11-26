19f5dcd471699a79ebfbf3f2b36716aea626b813
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-26 16:41:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,22 @@
 class Triangle
 
-  attr_accessor :equilateral, :isosceles, :scalene
+  attr_accessor :kind
 
-  def initialize
-    @equilateral = equilateral
-  end
-  # write code here
+  def initialize(a, b, c)
+    if a <= 0 || b <= 0 || c <= 0
+       raise TriangleError
+     elsif a + b <= c || b + c <= a || a + c <= b
+       raise TriangleError
+     elsif a == b && a == c
+       @kind = :equilateral
+     elsif (a == b || b == c || a == c)
+       @kind = :isosceles
+     elsif (a !=b && a != c && b != c)
+       @kind = :scalene
+     end
+   end
+ 
+   class TriangleError < StandardError
+   end
+ end
 
-
-  class TriangleError < StandardError
-  end
-end
